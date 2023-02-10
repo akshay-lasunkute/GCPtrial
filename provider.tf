8ab06e2ba5f77f65995f89411e2b041a8102933c
Akshay <akshay_lasunkute@persistent.com>
2023-02-10 10:59:54 +0530

    first commit

---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "google" {
+    #path for GCP service account credentials
+    credentials = "C:/Users/akshay_lasunkute/Downloads/inavolan-f2da9ac26c4a.json"
+    # GCP project ID
+    project     = "inavolan"
+    # Any region of your choice
+    region      = "us-central1"
+    # Any zone of your choice      
+    zone        = "us-central1-c"
+  }
+  
